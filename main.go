3cb985afe91c67b3a85a7a440e5f3a74438ee376
ctw <13834272287@163.com>
2022-05-12 10:21:25 +0800

    04_添加区块AddBlock

--- main.go
+++ main.go
@@ -58,12 +58,22 @@ func GensisBlock() *Block {
 	return NewBlock("创世块", []byte{})
 }
 
+// AddBlock 6. 添加区块
+func (bc *BlockChain) AddBlock(data string) {
+	lastBlock := bc.blocks[len(bc.blocks)-1]
+	prevHash := lastBlock.Hash
+	block := NewBlock(data, prevHash)
+	bc.blocks = append(bc.blocks, block)
+}
+
 func main() {
 	bc := NewBlockChain()
+	bc.AddBlock("班长向班花转了50枚比特币")
+	bc.AddBlock("班长又向班花转了50枚比特币")
 	for i, block := range bc.blocks {
 		fmt.Printf("====== 当前区块高度：%d =======\n", i)
 		fmt.Printf("前区块哈希值：%x\n", block.PrevHash)
 		fmt.Printf("当前区块哈希值：%x\n", block.Hash)
-		fmt.Printf("当前区块数据：%s\n", block.Data)
+		fmt.Printf("当前区块数据：%s\n\n", block.Data)
 	}
 }
